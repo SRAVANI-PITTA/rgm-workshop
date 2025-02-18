5ed8aab40fe33ce7819ec76d47cb16992a01c953
Pitta Sravani <sravanipitta7013@gmail.com>
2025-02-18 21:58:55 +0530

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,14 @@
+#inlude<stdio.h>
+{
+    int num;
+    printf("enter a number");
+    scanf("%d".&num);
+    if(num%!=0)
+    {
+        printf("%d is an odd number,\n",num);
+    }
+    else
+    {
+        printf("%d is an even number.\n",num);
+    }
+}
